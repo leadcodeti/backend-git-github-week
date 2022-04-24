5c3488ae7f0f8aedab39f094ba56c2b598cc0261
leonardocosta <sir.costa@yahoo.com.br>
2022-04-24 18:32:24 -0300

    Criação de User no Banco de Dados funcional.

--- src/main/java/br/com/leadcodeti/backend_git_github_week/controller/RegisterController.java
+++ src/main/java/br/com/leadcodeti/backend_git_github_week/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package br.com.leadcodeti.backend_git_github_week.controller;
 
+import br.com.leadcodeti.backend_git_github_week.model.User;
 import br.com.leadcodeti.backend_git_github_week.model.dto.UserDto;
 import br.com.leadcodeti.backend_git_github_week.model.dto.form.RegisterData;
+import br.com.leadcodeti.backend_git_github_week.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/register")
 public class RegisterController {
 
+    @Autowired
+    RegisterService registerService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<UserDto> register(@RequestBody RegisterData registerData) {
-        return ResponseEntity.ok(new UserDto(1, registerData.getName(), registerData.getEmail()));
+        User usuario = registerService.salvar(registerData);
+        return ResponseEntity.ok(new UserDto(usuario.getId(), usuario.getName(), usuario.getEmail()));
     }
 }
