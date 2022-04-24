d89f2473370307cf40c5509166604359aa4db9de
leonardocosta <sir.costa@yahoo.com.br>
2022-04-24 17:51:36 -0300

    Criação do 'esqueleto' de RegisterController.

---
+++ src/main/java/br/com/leadcodeti/backend_git_github_week/Controller/RegisterController.java
@@ -0,0 +1,13 @@
+package br.com.leadcodeti.backend_git_github_week.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+    public String register(String registerData) {
+        return "em construção...";
+    }
+}
