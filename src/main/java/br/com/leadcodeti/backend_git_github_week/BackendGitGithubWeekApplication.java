c615275e0c798095719f6203d5e468a3de254444
leonardocosta <sir.costa@yahoo.com.br>
2022-04-24 16:38:19 -0300

    Commit inicial: configuração inicial em pom.xml e banco de dados.

---
+++ src/main/java/br/com/leadcodeti/backend_git_github_week/BackendGitGithubWeekApplication.java
@@ -0,0 +1,13 @@
+package br.com.leadcodeti.backend_git_github_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGitGithubWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGitGithubWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leadcodeti/backend_git_github_week/BackendGitGithubWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leadcodeti.backend_git_github_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGitGithubWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
