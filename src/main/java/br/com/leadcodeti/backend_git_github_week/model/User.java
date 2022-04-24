3f0bd11858bcf3bc7dd6ed8e494be50d0ff19957
leonardocosta <sir.costa@yahoo.com.br>
2022-04-24 16:44:39 -0300

    Criando associacao entre User e Role na classe User.

--- src/main/java/br/com/leadcodeti/backend_git_github_week/model/User.java
+++ src/main/java/br/com/leadcodeti/backend_git_github_week/model/User.java
@@ -3,6 +3,7 @@ package br.com.leadcodeti.backend_git_github_week.model;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class User implements Serializable {
@@ -10,4 +11,5 @@ public class User implements Serializable {
     private String nome;
     private String email;
     private String senha;
+    private List<Role> roles;
 }
