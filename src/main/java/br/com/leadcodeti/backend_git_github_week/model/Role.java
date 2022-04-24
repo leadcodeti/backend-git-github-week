dd3a52f1d5e99177e2c3becba50f77ebe1b04917
leonardocosta <sir.costa@yahoo.com.br>
2022-04-24 16:56:03 -0300

    Corrigindo falha de mapeamento na classe Role.

--- src/main/java/br/com/leadcodeti/backend_git_github_week/model/Role.java
+++ src/main/java/br/com/leadcodeti/backend_git_github_week/model/Role.java
@@ -2,12 +2,16 @@ package br.com.leadcodeti.backend_git_github_week.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.io.Serializable;
 import java.util.List;
 
+@Entity
 @Data
 public class Role implements Serializable {
+    @Id
     private Integer id;
     private String nome;
     @ManyToMany(mappedBy = "roles")
